7c487e8314a411882f24376d903d5141028d6e68
khoa <user.iamjasper@gmail.com>
2024-11-09 03:34:41 +0700

    add authentication modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import SideBar from '@/components/SideBar'
 import SupabaseProvider from '@/Providers/SupabaseProvider'
 import UserProvider from '@/Providers/UserProvider'
+import ModalProvider from '@/Providers/ModalProvider'
+import ToasterProvider from '@/Providers/ToasterProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -20,8 +22,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={font.className}>
+                <ToasterProvider />
                 <SupabaseProvider>
                     <UserProvider>
+                        <ModalProvider />
                         <SideBar>{children}</SideBar>
                     </UserProvider>
                 </SupabaseProvider>
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,11 @@ import { HiHome } from 'react-icons/hi'
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx'
 import { twMerge } from 'tailwind-merge'
 import Button from './Button'
+import useAuthModal from '@/hooks/useAuthModal'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useUser } from '@/hooks/useUser'
+import { FaUserAlt } from 'react-icons/fa'
+import toast from 'react-hot-toast'
 
 interface HeaderProps {
     children: React.ReactNode
@@ -13,9 +18,20 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+    const authModal = useAuthModal()
     const router = useRouter()
+    const supabaseClient = useSupabaseClient()
+    const { user } = useUser()
 
-    const handleLogout = () => {}
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut()
+        router.refresh()
+        if (error) {
+            toast.error(error.message)
+        } else {
+            toast.success('Logged out!')
+        }
+    }
 
     return (
         <div
@@ -48,24 +64,41 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
                     </button>
                 </div>
                 <div className="flex justify-between items-center gap-x-4">
-                    <>
-                        <div>
+                    {user ? (
+                        <div className="flex gap-x-4 items-center">
                             <Button
-                                onClick={() => {}}
-                                className="bg-transparent text-neutral-300 font-medium"
-                            >
-                                Sign up
-                            </Button>
-                        </div>
-                        <div>
-                            <Button
-                                onClick={() => {}}
+                                onClick={handleLogout}
                                 className="bg-white px-6 py-2"
                             >
-                                Log in
+                                Logout
+                            </Button>
+                            <Button
+                                onClick={() => router.push('/account')}
+                                className="bg-white"
+                            >
+                                <FaUserAlt />
                             </Button>
                         </div>
-                    </>
+                    ) : (
+                        <>
+                            <div>
+                                <Button
+                                    onClick={authModal.onOpen}
+                                    className="bg-transparent text-neutral-300 font-medium"
+                                >
+                                    Sign up
+                                </Button>
+                            </div>
+                            <div>
+                                <Button
+                                    onClick={authModal.onOpen}
+                                    className="bg-white px-6 py-2"
+                                >
+                                    Log in
+                                </Button>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
             {children}
