2d669f0bcb25c9fb000e5da98728c83c86ccc96f
khoa <user.iamjasper@gmail.com>
2024-11-11 11:15:04 +0700

    feat: favorite

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -18,12 +18,15 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
     }
 
     return (
-        <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4 ">
+        <button
+            onClick={onClick}
+            className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4 "
+        >
             <div className="relative min-h-[64px] min-w-[64px]">
                 <Image
                     className="object-cover"
                     fill
-                    src="/images/liked.png"
+                    src={image}
                     alt="list item"
                 />
             </div>
